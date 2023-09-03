65c4bcbdef360679b790d09aab008537ab80f38c
mkhkshibata <mkhk.shibata@gmail.com>
2023-09-04 00:26:47 +0900

    chapter3 終了

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,13 +1,38 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+
+  def setup
+    @base_title = 'Ruby on Rails Tutorial Sample App'
   end
 
-  test "should get help" do
+  test "rootページのGETリクエストとタイトル表示のテスト" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "homeページのGETリクエストとタイトル表示のテスト" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@base_title}"
+  end
+
+  test "helpページのGETリクエストとタイトル表示のテスト" do
     get static_pages_help_url
     assert_response :success
+    assert_select "title", "Help | #{@base_title}"
+  end
+
+  test "aboutページのGETリクエストとタイトル表示のテスト" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@base_title}"
+  end
+
+  test "contactページのGETリクエストとタイトル表示のテスト" do
+    get static_pages_contact_url
+    assert_response :success
+    assert_select "title", "Contact | #{@base_title}"
   end
 end
